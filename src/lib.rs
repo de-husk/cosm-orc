b2be8d49552868810b3ac174f46fbb27bcab5853
Harry Hull <harry.hull1@gmail.com>
2022-06-22 14:53:04 -0700

    Fix doctests

--- src/lib.rs
+++ src/lib.rs
@@ -15,31 +15,32 @@
 //! # Quick Start
 //!
 //! ```
-//! use cosm_orc::{
-//!    config::config::Config,
-//!    orchestrator::cosm_orc::{CosmOrc, WasmMsg},
-//! };
-//! use cw20::Cw20Coin;
-//! use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+//! # use anyhow::Result;
+//! # use cosm_orc::{
+//! #    config::config::Config,
+//! #    orchestrator::cosm_orc::{CosmOrc, WasmMsg},
+//! # };
+//! # use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+//! # fn main() -> Result<()> {
+//!     // config.yaml has the cw20_base code_id already stored
+//!     // If the smart contract has not been stored on the chain yet use: `cosm_orc::store_contracts()`
+//!     let mut cosm_orc = CosmOrc::new(Config::from_yaml("./examples/juno_cfg.yaml")?);
 //!
-//! // config.yaml has the cw20_base code_id already stored
-//! // If the smart contract has not been stored on the chain yet use: `cosm_orc::store_contracts()`
-//! let mut cosm_orc = CosmOrc::new(Config::from_yaml("config.yaml")?);
+//!     let msgs: Vec<WasmMsg<InstantiateMsg, ExecuteMsg, QueryMsg>> = vec![
+//!         WasmMsg::InstantiateMsg(InstantiateMsg {
+//!             name: "Meme Token".to_string(),
+//!             symbol: "MEME".to_string(),
+//!             decimals: 6,
+//!             initial_balances: vec![],
+//!             mint: None,
+//!             marketing: None,
+//!         }),
+//!         WasmMsg::QueryMsg(QueryMsg::TokenInfo {}),
+//!     ];
 //!
-//! let msgs: Vec<WasmMsg<InstantiateMsg, ExecuteMsg, QueryMsg>> = vec![
-//!     WasmMsg::InstantiateMsg(InstantiateMsg {
-//!         name: "Meme Token".to_string(),
-//!         symbol: "MEME".to_string(),
-//!         decimals: 6,
-//!         initial_balances: vec![],
-//!         mint: None,
-//!         marketing: None,
-//!     }),
-//!     WasmMsg::QueryMsg(QueryMsg::TokenInfo {}),
-//! ];
-//!
-//! cosm_orc.process_msgs("cw20_base".to_string(), &msgs)?;
-//!
+//!     cosm_orc.process_msgs("cw20_base".to_string(), &msgs)?;
+//! #    Ok(())
+//! # }
 //! ```
 
 pub mod orchestrator;
