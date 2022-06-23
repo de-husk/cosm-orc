c55cb955f09be62b82aa37ff9d2dfd2922bf95f8
Harry Hull <harry.hull1@gmail.com>
2022-06-23 13:21:20 -0700

    Dont make users configure tx_flags (#5)
    * Dont make users configure tx_flags
    
    * Fix clippy

--- src/config/cfg.rs
+++ src/config/cfg.rs
@@ -3,10 +3,15 @@ use config::Config as _Config;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct Config {
-    pub chain_cfg: ChainConfig,
-    pub tx_flags: Vec<String>,
+    pub(crate) tx_flags: Vec<String>,
+    cfg: Cfg,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Cfg {
+    pub chain_cfg: ChainCfg,
     // used to configure already stored code_id dependencies
     // TODO: Just switch out `u64` for `DeployInfo` to allow users to already have the contract addr configured as well
     pub code_ids: HashMap<String, u64>,
@@ -15,7 +20,7 @@ pub struct Config {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ChainConfig {
+pub struct ChainCfg {
     pub binary: String,
     pub denom: String,
     pub chain_id: String,
@@ -29,8 +34,41 @@ impl Config {
             .add_source(config::Environment::with_prefix("GAS"))
             .build()?;
 
-        let cfg = settings.try_deserialize::<Config>()?;
+        let cfg = settings.try_deserialize::<Cfg>()?;
 
-        Ok(cfg)
+        Ok(Config {
+            tx_flags: Self::build_tx_flags(&cfg),
+            cfg,
+        })
+    }
+
+    fn build_tx_flags(cfg: &Cfg) -> Vec<String> {
+        //TODO: Allow more of this to be configured
+        vec![
+            "--gas-prices".to_string(),
+            format!("0.1{}", cfg.chain_cfg.denom),
+            "--gas".to_string(),
+            "auto".to_string(),
+            "--gas-adjustment".to_string(),
+            "1.5".to_string(),
+            "-b".to_string(),
+            "block".to_string(),
+            "--chain-id".to_string(),
+            cfg.chain_cfg.chain_id.clone(),
+            "--node".to_string(),
+            cfg.chain_cfg.rpc_endpoint.clone(),
+            "--from".to_string(),
+            cfg.key_name.clone(),
+            "--output".to_string(),
+            "json".to_string(),
+            "-y".to_string(),
+        ]
+    }
+}
+
+impl std::ops::Deref for Config {
+    type Target = Cfg;
+    fn deref(&self) -> &Self::Target {
+        &self.cfg
     }
 }
